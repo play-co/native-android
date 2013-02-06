94825e8f178a23e6095007e3978a8d13464f2bd9
DerekBurch <derek@gameclosure.com>
2013-02-06 04:10:31 -0800

    Initial changes to get the android code building on windows. Symlinks are now generated when running the install script.

--- install.sh
+++ install.sh
@@ -3,7 +3,11 @@
 NDK_MESSAGE="\033[1;31mERROR\033[0m: You must install android and android-ndk. \n\
 Then, return to where you installed native-android, and run ./install.sh"
 
-command -v android >/dev/null 2>&1 || { echo -e $NDK_MESSAGE; exit 1; }
+if [[ `uname` == MINGW32* ]]; then	
+	command -v android.bat >/dev/null 2>&1 || { echo -e $NDK_MESSAGE; exit 1; }
+else
+	command -v android >/dev/null 2>&1 || { echo -e $NDK_MESSAGE; exit 1; }
+fi
 command -v ndk-build >/dev/null 2>&1 || { echo -e $NDK_MESSAGE; exit 1; }
 
 remoteurl=`git config --get remote.origin.url`
