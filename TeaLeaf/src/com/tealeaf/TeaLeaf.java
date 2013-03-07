ccb6b8110c5e10f67c69faae5d5aa61526386231
Jared Petker <jared@gameclosure.com>
2013-03-07 12:21:15 -0800

    plugin consumation of back pressed fixed

--- TeaLeaf/src/com/tealeaf/TeaLeaf.java
+++ TeaLeaf/src/com/tealeaf/TeaLeaf.java
@@ -468,9 +468,12 @@ public class TeaLeaf extends FragmentActivity {
 	public void onBackPressed() {
 		Object [] objs = PluginManager.callAll("consumeOnBackPressed");
 
-		boolean consume = false;
+		boolean consume = true;
 		for (Object o : objs) {
-			 consume |= ((Boolean) o).booleanValue();
+			if (((Boolean) o).booleanValue()) {
+				consume = false;
+				break;
+			}
 		}
 
 		if (consume) {
