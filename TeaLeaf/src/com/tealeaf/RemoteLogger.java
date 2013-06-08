0a80b4d255a4f3aa1ad8b6b6cced35bbcf736249
Justin Sermeno <justin.sermeno@gmail.com>
2013-06-07 17:02:55 -0700

    comment out remote logger calls

--- TeaLeaf/src/com/tealeaf/RemoteLogger.java
+++ TeaLeaf/src/com/tealeaf/RemoteLogger.java
@@ -212,7 +212,7 @@ public class RemoteLogger implements ILogger {
 
 	@Override
 	public void log(Exception e) {
-		if(!logging) {
+		/*if(!logging) {
 			// only try to call log once--if the first one fails, screw it, otherwise we could
 			// end up with infinite recursion
 			logging = true;
@@ -223,7 +223,7 @@ public class RemoteLogger implements ILogger {
 			}
 			sendErrorEvent(context, payload.toString());
 			logging = false;
-		}
+		}*/
 	}
 
 	@Override
@@ -252,9 +252,9 @@ public class RemoteLogger implements ILogger {
 	}
 
 	private void send(LogEvent event) {
-		if (event != null) {
+		/*if (event != null) {
 			pool.execute(new RemoteLogSender(event));
-		}
+		}*/
 	}
 
 	private void enqueue(String event) {
@@ -283,6 +283,6 @@ public class RemoteLogger implements ILogger {
 	}
 
 	private void send(String event) throws Exception {
-		http.post(url, event);
+		//http.post(url, event);
 	}
 }
