966fd67d6ba630353d9ca26fb05610d896ccca6b
Chris A. Taylor <chris@gameclosure.com>
2013-08-07 17:57:27 -0700

    Remove extraneous argument

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ exports.load = function(common) {
 	
 	common.config.write();
 
-	require(common.paths.root('src', 'testapp')).registerTarget("native-android", __dirname, "build");
+	require(common.paths.root('src', 'testapp')).registerTarget("native-android", __dirname);
 }
 
 exports.testapp = function(common, opts, next) {
